5614faf0a5b4447a3f85ec535e06a5e5cd34d2aa
SValac <shiro.valac@gmail.com>
2023-12-10 04:55:42 -0600

    preventing user from leaving the page with onBeforerouteLeave

--- src/pages/Invoices.vue
+++ src/pages/Invoices.vue
@@ -1,3 +1,16 @@
+<script setup>
+// functionto know when user is leaving the current page
+import { onBeforeRouteLeave } from 'vue-router';
+
+// takes a callback function that execute befre the use rnavigates to the next route
+onBeforeRouteLeave((to, from) => {
+	// retun false forn this function prevest the navigation from happening
+	const answer = window.confirm('Are you sure you want to leave?');
+
+	if (!answer) return false;
+});
+</script>
+
 <template>
 	<div>
 		<h1>Invoices</h1>
